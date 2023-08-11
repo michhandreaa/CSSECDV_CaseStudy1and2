5de8be5c15180412a388c90564982e08ff39441a
michhandreaa <91170055+michhandreaa@users.noreply.github.com>
2023-08-11 23:57:47 +0800

    Manager tweaks

--- src/View/ManagerHome.java
+++ src/View/ManagerHome.java
@@ -52,7 +52,7 @@ public class ManagerHome extends javax.swing.JPanel {
 //        historyBtn.setVisible(false);
 //        usersBtn.setVisible(false);
 //        productsBtn.setVisible(false);
-//        logsBtn.setVisible(false);
+        logsBtn.setEnabled(false);
     }
     
     public void showPnl(String panelName){
